83aac1835a17bfff82b96957b0d971c50cb3d294
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-02 16:59:30 +0100

    Add: react on server connection lost ===: Visualize and Quit after some seconds

--- 5-example-projects/godot4-multiplayer-template-cs-main/client/ClientManager.cs
+++ 5-example-projects/godot4-multiplayer-template-cs-main/client/ClientManager.cs
@@ -5,101 +5,112 @@ using MessagePack;
 // Code executed on the client side only, handles network events
 public partial class ClientManager : Node
 {
-    [Export] private string _address = "localhost";
-    [Export] private int _port = 9999;
-    [Export] private int _lerpBufferWindow = 50;
-    [Export] private int _maxLerp = 150;
+	[Export] private string _address = "localhost";
+	[Export] private int _port = 9999;
+	[Export] private int _lerpBufferWindow = 50;
+	[Export] private int _maxLerp = 150;
 
-    private SceneMultiplayer _multiplayer = new();
-    private SnapshotInterpolator _snapshotInterpolator = new();
-    private NetworkClock _netClock;
-    private Node _entityArray;
+	private SceneMultiplayer _multiplayer = new();
+	private SnapshotInterpolator _snapshotInterpolator = new();
+	private NetworkClock _netClock;
+	private Node _entityArray;
 
-    // Debug only
-    private double _sentPerSecond = 0, _recPerSecond = 0, _packetsPerSecond = 0, _sentPacketsPerSecond = 0;
+	// Debug only
+	private double _sentPerSecond = 0, _recPerSecond = 0, _packetsPerSecond = 0, _sentPacketsPerSecond = 0;
 
-    public override void _Ready()
-    {
-        Connect();
+	public override void _Ready()
+	{
+		Connect();
 
-        _entityArray = GetNode("/root/Main/EntityArray");
+		_entityArray = GetNode("/root/Main/EntityArray");
 
-        _netClock = GetNode<NetworkClock>("NetworkClock");
-        _netClock.Initialize(_multiplayer);
-        _netClock.LatencyCalculated += OnLatencyCalculated;
-    }
+		_netClock = GetNode<NetworkClock>("NetworkClock");
+		_netClock.Initialize(_multiplayer);
+		_netClock.LatencyCalculated += OnLatencyCalculated;
+	}
 
-    public override void _Process(double delta)
-    {
-        _snapshotInterpolator.InterpolateStates(_entityArray, NetworkClock.Clock);
-        DebugInfo(delta);
-    }
+	public override void _Process(double delta)
+	{
+		_snapshotInterpolator.InterpolateStates(_entityArray, NetworkClock.Clock);
+		DebugInfo(delta);
+	}
 
-    private void OnPacketReceived(long id, byte[] data)
-    {
-        var command = MessagePackSerializer.Deserialize<NetMessage.ICommand>(data);
+	private void OnPacketReceived(long id, byte[] data)
+	{
+		var command = MessagePackSerializer.Deserialize<NetMessage.ICommand>(data);
 
-        if (command is NetMessage.GameSnapshot snapshot)
-        {
-            _snapshotInterpolator.PushState(snapshot);
+		if (command is NetMessage.GameSnapshot snapshot)
+		{
+			_snapshotInterpolator.PushState(snapshot);
 
-            foreach (NetMessage.UserState state in snapshot.States)
-            {
-                if (state.Id == Multiplayer.GetUniqueId())
-                {
-                    CustomSpawner.LocalPlayer.ReceiveState(state);
-                }
-            }
-        }
-    }
+			foreach (NetMessage.UserState state in snapshot.States)
+			{
+				if (state.Id == Multiplayer.GetUniqueId())
+				{
+					CustomSpawner.LocalPlayer.ReceiveState(state);
+				}
+			}
+		}
+	}
 
-    private void OnLatencyCalculated(int latencyAverage, int offsetAverage, int jitter)
-    {
-        _snapshotInterpolator.BufferTime = Mathf.Clamp(latencyAverage + _lerpBufferWindow, 0, _maxLerp);
-    }
+	private void OnLatencyCalculated(int latencyAverage, int offsetAverage, int jitter)
+	{
+		_snapshotInterpolator.BufferTime = Mathf.Clamp(latencyAverage + _lerpBufferWindow, 0, _maxLerp);
+	}
 
-    private void OnConnectedToServer()
-    {
-        GetNode<Label>("Debug/Label").Text += $"\n{Multiplayer.GetUniqueId()}";
-    }
+	private void OnConnectedToServer()
+	{
+		GetNode<Label>("Debug/Label").Text += $"\n{Multiplayer.GetUniqueId()}";
+	}
 
-    private void Connect()
-    {
-        _multiplayer.ConnectedToServer += OnConnectedToServer;
-        _multiplayer.PeerPacket += OnPacketReceived;
+	private void Connect()
+	{
+		_multiplayer.ConnectedToServer += OnConnectedToServer;
+		_multiplayer.PeerPacket += OnPacketReceived;
+		_multiplayer.ServerDisconnected += OnServerDisconnected;
 
-        ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
-        peer.CreateClient(_address, _port);
-        _multiplayer.MultiplayerPeer = peer;
-        GetTree().SetMultiplayer(_multiplayer);
-        GD.Print("Client connected to ", _address, ":", _port);
-    }
+		ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
+		peer.CreateClient(_address, _port);
+		_multiplayer.MultiplayerPeer = peer;
+		GetTree().SetMultiplayer(_multiplayer);
+		GD.Print("Client connected to ", _address, ":", _port);
+	}
 
-    private void DebugInfo(double delta)
-    {
-        var label = GetNode<Label>("Debug/Label2");
-        label.Modulate = Colors.White;
+	private async void OnServerDisconnected()
+	{
+		// ToDo: Quit and Stop all running processes
+		_multiplayer = null;
+		GetNode<Label>("Debug/Label").Modulate = Colors.Red;
+		GetNode<Label>("Debug/Label").Text = $"\nServer has closed the Connection !\nQuitting in 5 Seconds";
+		await ToSignal(GetTree().CreateTimer(5), "timeout");
+		GetTree().Quit();
+	}
 
-        label.Text = $"buf {_snapshotInterpolator.BufferCount} ";
-        label.Text += String.Format("int {0:0.00}", _snapshotInterpolator.InterpolationFactor);
-        label.Text += $" len {_snapshotInterpolator.BufferTime}ms \nclk {NetworkClock.Clock} ofst {_netClock.Offset}ms";
-        label.Text += $"\nping {_netClock.InmediateLatency}ms r_pps {_packetsPerSecond} t_pps {_sentPacketsPerSecond} jit {_netClock.Jitter}";
+	private void DebugInfo(double delta)
+	{
+		var label = GetNode<Label>("Debug/Label2");
+		label.Modulate = Colors.White;
 
-        if (CustomSpawner.LocalPlayer != null)
-        {
-            label.Text += $"\nrdt {CustomSpawner.LocalPlayer.RedundantInputs} tx {_sentPerSecond} rx {_recPerSecond}";
-        }
+		label.Text = $"buf {_snapshotInterpolator.BufferCount} ";
+		label.Text += String.Format("int {0:0.00}", _snapshotInterpolator.InterpolationFactor);
+		label.Text += $" len {_snapshotInterpolator.BufferTime}ms \nclk {NetworkClock.Clock} ofst {_netClock.Offset}ms";
+		label.Text += $"\nping {_netClock.InmediateLatency}ms r_pps {_packetsPerSecond} t_pps {_sentPacketsPerSecond} jit {_netClock.Jitter}";
 
-        if (_snapshotInterpolator.InterpolationFactor > 1)
-            label.Modulate = Colors.Red;
-    }
+		if (CustomSpawner.LocalPlayer != null)
+		{
+			label.Text += $"\nrdt {CustomSpawner.LocalPlayer.RedundantInputs} tx {_sentPerSecond} rx {_recPerSecond}";
+		}
 
-    private void OnDebugTimerOut()
-    {
-        var enetHost = (Multiplayer.MultiplayerPeer as ENetMultiplayerPeer).Host;
-        _sentPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.SentData);
-        _recPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.ReceivedData);
-        _packetsPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.ReceivedPackets);
-        _sentPacketsPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.SentPackets);
-    }
+		if (_snapshotInterpolator.InterpolationFactor > 1)
+			label.Modulate = Colors.Red;
+	}
+
+	private void OnDebugTimerOut()
+	{
+		var enetHost = (Multiplayer.MultiplayerPeer as ENetMultiplayerPeer).Host;
+		_sentPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.SentData);
+		_recPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.ReceivedData);
+		_packetsPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.ReceivedPackets);
+		_sentPacketsPerSecond = enetHost.PopStatistic(ENetConnection.HostStatistic.SentPackets);
+	}
 }
