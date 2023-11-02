c36a156159e68ecf807c95295fb7d1bbaeaa30e7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-02 15:58:24 +0100

    Rem: Rotation of qube, its only localy in all instances

--- 5-example-projects/godot4-multiplayer-template-cs-main/debug/CubeRotation.gd
+++ 5-example-projects/godot4-multiplayer-template-cs-main/debug/CubeRotation.gd
@@ -1,4 +1,8 @@
 extends MeshInstance3D
 
-func _process(delta):
-    self.rotate_y(delta * 4)
+func _process(_delta):
+	#if multiplayer.is_server():
+		#self.rotate_y(delta * 4)
+	#else:
+		#self.rotate_y(-delta * 4)
+	pass
