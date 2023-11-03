cdcdef8e9f7cbad89ea57686f0a5b975c5eac193
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-03 09:04:53 +0100

    Upd: Some code suggestions from vscode

--- 5-example-projects/godot4-multiplayer-template-cs-main/server/ServerManager.cs
+++ 5-example-projects/godot4-multiplayer-template-cs-main/server/ServerManager.cs
@@ -1,64 +1,57 @@
 using Godot;
 using System;
 using MessagePack;
+using System.Linq;
 
 // Code executed on the server side only, handles network events
 public partial class ServerManager : Node
 {
-    [Export] private int _port = 9999;
+	[Export] private int _port = 9999;
 
-    private SceneMultiplayer _multiplayer = new();
-    private Godot.Collections.Array<Godot.Node> entityArray;
+	private SceneMultiplayer _multiplayer = new();
+	private Godot.Collections.Array<Godot.Node> entityArray;
 
-    private const int NET_TICKRATE = 30; //hz
-    private double _netTickCounter = 0;
+	private const int NET_TICKRATE = 30; //hz
+	private double _netTickCounter = 0;
 
-    public override void _Ready()
-    {
-        Create();
-    }
+	public override void _Ready()
+	{
+		Create();
+	}
 
-    public override void _Process(double delta)
-    {
-        DebugInfo();
+	public override void _Process(double delta)
+	{
+		DebugInfo();
 
-        _netTickCounter += delta;
-        if (_netTickCounter >= (1.0 / NET_TICKRATE))
-        {
-            NetworkProcess();
-            _netTickCounter = 0;
-        }
-    }
+		_netTickCounter += delta;
+		if (_netTickCounter >= (1.0 / NET_TICKRATE))
+		{
+			NetworkProcess();
+			_netTickCounter = 0;
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        entityArray = GetNode("/root/Main/EntityArray").GetChildren();
-        ProcessPendingPackets();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		entityArray = GetNode("/root/Main/EntityArray").GetChildren();
+		ProcessPendingPackets();
+	}
 
-    private void NetworkProcess() // Called every NET_TICKRATE hz
-    {
-        BroadcastSnapshot();
-    }
+	private void NetworkProcess() // Called every NET_TICKRATE hz
+	{
+		BroadcastSnapshot();
+	}
 
-    // Process corresponding packets for this tick
-    private void ProcessPendingPackets()
-    {
-        foreach (ServerPlayer player in entityArray)
-        {
-            player.ProcessPendingCommands();
-        }
-    }
+	// Process corresponding packets for this tick
+	private void ProcessPendingPackets()
+	{
+		foreach (ServerPlayer player in entityArray.Cast<ServerPlayer>())
+		{
+			player.ProcessPendingCommands();
+		}
+	}
 
-    // Pack and send GameSnapshot with all entities and their information
-    private void BroadcastSnapshot()
-    {
-        var snapshot = new NetMessage.GameSnapshot
-        {
-            Time = (int)Time.GetTicksMsec(),
-            States = new NetMessage.UserState[entityArray.Count]
-        };
-
+	// Pack and send GameSnapshot with all entities and their information
 	private void BroadcastSnapshot()
 	{
 		var snapshot = new NetMessage.GameSnapshot
@@ -74,61 +67,65 @@ public partial class ServerManager : Node
 				snapshot.States[i] = player.GetCurrentState();
 		}
 
-        byte[] data = MessagePackSerializer.Serialize<NetMessage.ICommand>(snapshot);
+		byte[] data = MessagePackSerializer.Serialize<NetMessage.ICommand>(snapshot);
 
-        _multiplayer.SendBytes(data, 0,
-            MultiplayerPeer.TransferModeEnum.UnreliableOrdered, 0);
-    }
+		_multiplayer.SendBytes(data, 0,
+			MultiplayerPeer.TransferModeEnum.UnreliableOrdered, 0);
+	}
 
-    private void OnPacketReceived(long id, byte[] data)
-    {
-        var command = MessagePackSerializer.Deserialize<NetMessage.ICommand>(data);
+	private void OnPacketReceived(long id, byte[] data)
+	{
+		var command = MessagePackSerializer.Deserialize<NetMessage.ICommand>(data);
 
-        switch (command)
-        {
-            case NetMessage.UserCommand userCmd:
-                ServerPlayer player = GetNode($"/root/Main/EntityArray/{userCmd.Id}") as ServerPlayer;
-                player.PushCommand(userCmd);
-                break;
+		switch (command)
+		{
+			case NetMessage.UserCommand userCmd:
+				ServerPlayer player = GetNode($"/root/Main/EntityArray/{userCmd.Id}") as ServerPlayer;
+				player.PushCommand(userCmd);
+				break;
 
-            case NetMessage.Sync sync:
-                sync.ServerTime = (int)Time.GetTicksMsec();
-                _multiplayer.SendBytes(MessagePackSerializer.Serialize<NetMessage.ICommand>(sync), (int)id,
-                MultiplayerPeer.TransferModeEnum.Unreliable, 1);
-                break;
-        }
-    }
+			case NetMessage.Sync sync:
+				sync.ServerTime = (int)Time.GetTicksMsec();
+				_multiplayer.SendBytes(MessagePackSerializer.Serialize<NetMessage.ICommand>(sync), (int)id,
+				MultiplayerPeer.TransferModeEnum.Unreliable, 1);
+				break;
+		}
+	}
 
-    private void OnPeerConnected(long id)
-    {
-        Node playerInstance = GetNode<MultiplayerSpawner>("/root/Main/MultiplayerSpawner").Spawn(id);
-        GD.Print("Peer ", id, " connected");
-    }
+	private void OnPeerConnected(long id)
+	{
+		//Node playerInstance = 
+		GetNode<MultiplayerSpawner>("/root/Main/MultiplayerSpawner").Spawn(id);
+		GD.Print("Peer ", id, " connected");
+	}
 
-    private void OnPeerDisconnected(long id)
-    {
-        GetNode($"/root/Main/EntityArray/{id}").QueueFree();
-        GD.Print("Peer ", id, " disconnected");
-    }
+	private void OnPeerDisconnected(long id)
+	{
+		GetNode($"/root/Main/EntityArray/{id}").QueueFree();
+		GD.Print("Peer ", id, " disconnected");
+	}
 
-    private void Create()
-    {
-        _multiplayer.PeerConnected += OnPeerConnected;
-        _multiplayer.PeerDisconnected += OnPeerDisconnected;
-        _multiplayer.PeerPacket += OnPacketReceived;
+	private void Create()
+	{
+		_multiplayer.PeerConnected += OnPeerConnected;
+		_multiplayer.PeerDisconnected += OnPeerDisconnected;
+		_multiplayer.PeerPacket += OnPacketReceived;
 
-        ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
-        peer.CreateServer(_port);
+		ENetMultiplayerPeer peer = new();
+		peer.CreateServer(_port);
 
-        _multiplayer.MultiplayerPeer = peer;
-        GetTree().SetMultiplayer(_multiplayer);
+		_multiplayer.MultiplayerPeer = peer;
+		// ToDo: Make a dedicated node for Server peer
+		//GetTree().SetMultiplayer(_multiplayer);
+		GD.Print("ServerManager::create() -> NodePath: " + GetPath());
+		GetTree().SetMultiplayer(_multiplayer, GetPath());
 
-        GD.Print("Server listening on ", _port);
-    }
+		GD.Print("Server listening on ", _port);
+	}
 
-    private void DebugInfo()
-    {
-        var label = GetNode<Label>("Debug/Label2");
-        label.Text = $"clk {Time.GetTicksMsec()}";
-    }
+	private void DebugInfo()
+	{
+		var label = GetNode<Label>("Debug/Label2");
+		label.Text = $"clk {Time.GetTicksMsec()}";
+	}
 }
--- 5-example-projects/godot4-multiplayer-template-cs-main/shared/CustomSpawner.cs
+++ 5-example-projects/godot4-multiplayer-template-cs-main/shared/CustomSpawner.cs
@@ -3,53 +3,55 @@ using System;
 
 public partial class CustomSpawner : MultiplayerSpawner
 {
-    [Export] private PackedScene _playerScene;
-    [Export] private PackedScene _serverPlayerScene;
-    [Export] private PackedScene _dummyScene;
+	[Export] private PackedScene _playerScene;
+	[Export] private PackedScene _serverPlayerScene;
+	[Export] private PackedScene _dummyScene;
 
-    public static ClientPlayer LocalPlayer;
+    private static ClientPlayer localPlayer;
+
+    public static ClientPlayer LocalPlayer { get => localPlayer; set => localPlayer = value; }
 
     public override void _Ready()
-    {
-        Callable customSpawnFunctionCallable = new Callable(this, nameof(CustomSpawnFunction));
-        this.SpawnFunction = customSpawnFunctionCallable;
+	{
+		Callable customSpawnFunctionCallable = new (this, nameof(CustomSpawnFunction));
+		this.SpawnFunction = customSpawnFunctionCallable;
 
-        this.SetMultiplayerAuthority(Multiplayer.GetUniqueId());
-    }
+		this.SetMultiplayerAuthority(Multiplayer.GetUniqueId());
+	}
 
-    private Node CustomSpawnFunction(Variant data)
-    {
-        int spawnedPlayerID = (int)data;
-        int localID = Multiplayer.GetUniqueId();
+	private Node CustomSpawnFunction(Variant data)
+	{
+		int spawnedPlayerID = (int)data;
+		int localID = Multiplayer.GetUniqueId();
 
-        // Server character for simulation
-        if (localID == 1)
-        {
-            GD.Print("Spawned server character");
-            ServerPlayer player = _serverPlayerScene.Instantiate() as ServerPlayer;
-            player.Name = spawnedPlayerID.ToString();
-            player.MultiplayerID = spawnedPlayerID;
-            return player;
-        }
+		// Server character for simulation
+		if (localID == 1 && startmode == 2)
+		{
+			GD.Print("Spawned server character");
+			ServerPlayer player = _serverPlayerScene.Instantiate() as ServerPlayer;
+			player.Name = spawnedPlayerID.ToString();
+			player.MultiplayerID = spawnedPlayerID;
+			return player;
+		}
 
-        // Client player
-        if (localID == spawnedPlayerID)
-        {
-            GD.Print("Spawned client player");
-            ClientPlayer player = _playerScene.Instantiate() as ClientPlayer;
-            player.Name = spawnedPlayerID.ToString();
-            player.SetMultiplayerAuthority(spawnedPlayerID);
-            LocalPlayer = player;
-            return player;
-        }
+		// Client player
+		if (localID == spawnedPlayerID)
+		{
+			GD.Print("Spawned client player");
+			ClientPlayer player = _playerScene.Instantiate() as ClientPlayer;
+			player.Name = spawnedPlayerID.ToString();
+			player.SetMultiplayerAuthority(spawnedPlayerID);
+			LocalPlayer = player;
+			return player;
+		}
 
-        // Dummy player
-        {
-            GD.Print("Spawned dummy");
-            Node player = _dummyScene.Instantiate();
-            player.Name = spawnedPlayerID.ToString();
-            player.SetMultiplayerAuthority(spawnedPlayerID);
-            return player;
-        }
-    }
+		// Dummy player
+		{
+			GD.Print("Spawned dummy");
+			Node player = _dummyScene.Instantiate();
+			player.Name = spawnedPlayerID.ToString();
+			player.SetMultiplayerAuthority(spawnedPlayerID);
+			return player;
+		}
+	}
 }
