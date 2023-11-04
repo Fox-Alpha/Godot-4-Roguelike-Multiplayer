d47072351aa9f1c02b5e97fe9b6bc4a786804778
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-04 16:38:49 +0100

    Chg: Only refresh connected clients list when 1 or more connected

--- 5-example-projects/godot4-multiplayer-template-cs-main/debug/debug_label.gd
+++ 5-example-projects/godot4-multiplayer-template-cs-main/debug/debug_label.gd
@@ -1,8 +1,9 @@
 extends Label
 
 func _process(_delta):
-	var entities = get_node("/root/Main/EntityArray").get_children()
-
 	self.text = ""
-	for entity in entities:
-		self.text += entity.name + " " + str(entity.position.snapped(Vector3.ONE*0.1)) + "\n"
+	if $"/root/Main/ServerAuthority/EntityArray".get_child_count() > 0:
+		var entities = get_node("/root/Main/ServerAuthority/EntityArray").get_children()
+
+		for entity in entities:
+			self.text += entity.name + " " + str(entity.position.snapped(Vector3.ONE*0.1)) + "\n"
