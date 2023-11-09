92bcb7c723af3704f54d678895eb9c000117dc5d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-09 17:28:13 +0100

    Add: func for gd debug at runtime (_process)

--- 5-example-projects/Godot4-DotNet-VSC-Config-Test/main.gd
+++ 5-example-projects/Godot4-DotNet-VSC-Config-Test/main.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var deltatime : float = 0.0
 
 func _ready() -> void:
 	DisplayServer.window_set_title("VSC Test Configuration ... runing")
+
+func _process(delta: float) -> void:
+	if deltatime >= 1:
+		print(deltatime)
+		deltatime = 0.0
+
+	deltatime += delta
