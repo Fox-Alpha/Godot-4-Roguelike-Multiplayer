0d2bb7f7a219b1b0efa40994d1e1dcbf6c527db3
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-10 09:42:13 +0100

    Add: Position Label updating in runtime Add: Export variables Upd: Moving in all directions

--- 5-example-projects/Godot4-DotNet-VSC-Config-Test/Character.cs
+++ 5-example-projects/Godot4-DotNet-VSC-Config-Test/Character.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class Character : Godot.CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export] public const float Speed = 300.0f;
+	[Export] public const float JumpVelocity = -400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	//public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -29,15 +29,23 @@ public partial class Character : Godot.CharacterBody2D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("ui_left", 
+			"ui_right", 
+			"ui_up", 
+			"ui_down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity = (direction * Speed);
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
+
+		Label pos = this.GetNodeOrNull<Label>("../UI/LabelPosition");
+		if(pos is not null)
+			pos.Text = $"Pos: (X: {GlobalPosition.X:F} / Y: {GlobalPosition.Y:F})";
 
 		Velocity = velocity;
 		MoveAndSlide();
