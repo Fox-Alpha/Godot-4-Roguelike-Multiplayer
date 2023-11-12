99e56bf4387098898ce8ce4f6b381cdf3fb39797
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-12 22:55:25 +0100

    Upd: Comment unused func

--- 5-example-projects/godot4-multiplayer-template-cs-main/debug/CubeRotation.gd
+++ 5-example-projects/godot4-multiplayer-template-cs-main/debug/CubeRotation.gd
@@ -1,8 +1,8 @@
 extends MeshInstance3D
 
-func _process(_delta):
+#func _process(_delta):
 	#if multiplayer.is_server():
 		#self.rotate_y(delta * 4)
 	#else:
 		#self.rotate_y(-delta * 4)
-	pass
+	#pass
