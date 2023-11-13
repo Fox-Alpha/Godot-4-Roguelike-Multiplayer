b2e63245fda654d6f131cadf80e6edd7cee2c1bb
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-13 16:56:18 +0100

    Upd: Port gd Script to C# Script

---
+++ 5-example-projects/godot4-multiplayer-template-cs-main/scripts/MenuController.cs
@@ -0,0 +1,52 @@
+using Godot;
+using Godot.Collections;
+
+[GlobalClass]
+public partial class MenuController : Node
+{
+	[Export] private PackedScene _server_scene;
+	[Export] private PackedScene _client_scene;
+
+	private Control startbuttons;
+
+	public override void _Ready()
+	{
+		_server_scene = GD.Load<PackedScene>("res://scenes/Server.tscn");
+		_client_scene = GD.Load<PackedScene>("res://scenes/Client.tscn");
+
+		startbuttons = GetNodeOrNull<Control>("%Buttons");
+	}
+
+
+	private void _on_button_pressed(int host = 2)
+	{
+		string title = "";
+
+		if (host == 1)
+		{
+			Node svr_scn_inst = _server_scene.Instantiate();
+			this.AddChild(svr_scn_inst);
+			ToSignal(GetTree().CreateTimer(0.5), "timeout ");
+
+			title = "Dedicated Server";
+		}
+		else if (host == 2)
+		{
+			//Node svr_scn_inst = _server_scene.Instantiate();
+			this.AddChild(_server_scene.Instantiate());
+			ToSignal(GetTree().CreateTimer(0.5), "timeout ");
+
+			title = "Dedicated Server";
+		}
+		else
+		{
+			GetNode<Label>("Control/Label").Text = "Client only Side";
+			this.AddChild(_client_scene.Instantiate());
+			title = "Client";
+		}
+
+		DisplayServer.WindowSetTitle(title);
+		//$ServerAuthority/MultiplayerSpawner.startmode = host
+		startbuttons.QueueFree();
+	}
+}
