2fdd55d60523dd6e324da20e886940c5eafbae40
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-14 14:13:23 +0100

    Chg: Change rotation when Multiplayer not started

--- 5-example-projects/godot4-multiplayer-template-cs-main/debug/CubeRotation.cs
+++ 5-example-projects/godot4-multiplayer-template-cs-main/debug/CubeRotation.cs
@@ -3,16 +3,22 @@ using Godot;
 [GlobalClass]
 public partial class CubeRotation : MeshInstance3D
 {
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-        if (this.Multiplayer.IsServer())
-        {
-            this.RotateY((float)delta * 4);
-        }
-        else
-        {
-            this.RotateY((float)-delta * 4);
-        }
-    }
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		if(!Multiplayer.HasMultiplayerPeer())
+			this.RotateX((float)delta * 1);
+			this.RotateY((float)delta * 1);
+			this.RotateZ((float)delta * 1);
+			//return;
+
+		if (this.Multiplayer.IsServer())
+		{
+			this.RotateY((float)delta * 2);
+		}
+		else
+		{
+			this.RotateY((float)-delta * 2);
+		}
+	}
 }
