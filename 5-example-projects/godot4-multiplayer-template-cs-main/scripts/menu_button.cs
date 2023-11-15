9bdb63a5e8f680fd9ac702f8c7d4c2c7af0c6976
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-15 14:43:53 +0100

    Chg: Reload scene in _ExitTree Upd: Close peer connection gracefully

--- 5-example-projects/godot4-multiplayer-template-cs-main/scripts/menu_button.cs
+++ 5-example-projects/godot4-multiplayer-template-cs-main/scripts/menu_button.cs
@@ -1,4 +1,5 @@
 using Godot;
+using multiplayerbase.server;
 using System;
 
 public partial class menu_button : MenuButton
@@ -9,6 +10,12 @@ public partial class menu_button : MenuButton
         GetPopup().IdPressed += ExitMenuPresses;
     }
 
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+		GetTree().ReloadCurrentScene();
+    }
+
     private void ExitMenuPresses(long id)
 	{
 		GD.Print($"Menu {GetPopup().GetItemText((int)id)} ({id}) is pressed");
@@ -16,15 +23,29 @@ public partial class menu_button : MenuButton
 		switch (id)
 		{
 			case 0:		// Quit to Menu
-				// Todo: Add Buttons Back
-                Multiplayer.MultiplayerPeer.Close();
-				SceneMultiplayer mp = new();
+				// Todo: (WIP) Add Buttons Back
 				
-				GetTree().SetMultiplayer(mp);
-                //Multiplayer.MultiplayerPeer = null;
-                GetTree().ReloadCurrentScene();
-                //var own = GetOwner<Node>();
-				//own.QueueFree();
+				var peer = GetTree().GetMultiplayer().MultiplayerPeer;
+				Node parent = null;
+
+				if (GetTree().GetMultiplayer().IsServer())
+				{
+					parent = this.GetOwnerOrNull<ServerManager>();
+				}
+				else if (!GetTree().GetMultiplayer().IsServer())
+				{
+					parent = this.GetOwnerOrNull<ClientManager>();
+				}
+
+				GetTree().GetMultiplayer().MultiplayerPeer.Close();
+				
+				GetTree().SetMultiplayer(MultiplayerApi.CreateDefaultInterface());
+
+				// ToDo: What id parent(owner) is noct null but also noct client- or servermanager ?
+				if (parent != null)
+				{
+					parent.QueueFree();
+				}
 				break;
 			case 1:		// Quit App
 				GetTree().Quit();
