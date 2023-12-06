4cfd2a903677e734be55b881aac8b895ea03cef9
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-06 22:37:26 +0100

    Add: Set authority to peer for player instance Add: Set position to screen center

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scenes/custom_spawner.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scenes/custom_spawner.gd
@@ -20,4 +20,8 @@ func _OnCustomSpawn(data) -> Node:
 	print("OnCustomSpawn() : ", multiplayer.get_unique_id(), " : ", data)
 	var player : CharacterBody2D = PLAYER.instantiate()
 	player.name = str(data)
+	player.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	var pos = get_tree().current_scene.get_viewport().get_visible_rect().get_center()
+	player.global_position = pos
+	player.set_multiplayer_authority(data)
 	return player
