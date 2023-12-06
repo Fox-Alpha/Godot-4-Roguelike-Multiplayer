205d13a85adb3510e6d9c215b2ed574f9acbd8df
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-06 16:55:31 +0100

    Add: Synchronizer for position

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/objects/entities/player/player.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/objects/entities/player/player.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = -400.0
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity.y += gravity * delta
