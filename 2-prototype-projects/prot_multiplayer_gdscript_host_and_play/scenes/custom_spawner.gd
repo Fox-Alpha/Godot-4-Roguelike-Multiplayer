5730ce9ab6e910d206665f26dfb4db4a44659df0
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-07 16:56:19 +0100

    Chg: Adding dummy node only on server side Chg: adding the player only on peers

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scenes/custom_spawner.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scenes/custom_spawner.gd
@@ -16,12 +16,38 @@ func _process(_delta):
 	pass
 
 
-func _OnCustomSpawn(data) -> Node:
-	print("OnCustomSpawn() : ", multiplayer.get_unique_id(), " : ", data)
+func _OnCustomSpawn(data:int) -> Node:
+	var spawnedPlayerID:int = data
+	var localID : int = multiplayer.get_unique_id()
+
+	## DEBUG
+	#print("OnCustomSpawn() : ", multiplayer.get_unique_id(), " : ", data)
+	print(	""">> MultiplayerSpawner::CustomSpawnFunction():
+			Local UniqueId: ({%d}
+			Authority: {%d})""" % [multiplayer.get_unique_id(), get_multiplayer_authority()]);
+
+
+	if localID == 1:					# is local Server
+		print("Local Server")
+		var server : Node = Node.new()
+		server.name = str(data)
+
+		server.set_multiplayer_authority(1)
+		return server
+	elif localID == spawnedPlayerID:	# is local Player
+		print("local player")
+		pass
+	else:								# all other peers
+		print("other peer")
+		pass
+
 	var player : CharacterBody2D = PLAYER.instantiate()
 	player.name = str(data)
-	player.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	#player.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+
 	var pos = get_tree().current_scene.get_viewport().get_visible_rect().get_center()
 	player.global_position = pos
+
 	player.set_multiplayer_authority(data)
+
 	return player
