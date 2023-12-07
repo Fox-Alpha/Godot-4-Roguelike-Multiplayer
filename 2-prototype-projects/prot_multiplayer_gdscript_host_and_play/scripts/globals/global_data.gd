0005359010db2f563e44901f0955a5cad5ce05cc
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-07 22:47:58 +0100

    Add: Using global signal for network mode change Add: global const for port and Max Players Add: reference for debug label

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scripts/globals/global_data.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scripts/globals/global_data.gd
@@ -1,11 +1,27 @@
 extends Node
 
 enum NetworkMode {
-	CLIENTMODE,			# Client Only
-	SERVERMODE,			# Dedicated Server
+	CLIENTONLYMODE,			# Client Only
+	SERVERONLYMODE,			# Dedicated Server
 	SINGLEPLAYERMODE,	# Local Singleplayer Mode
 	SERVERCLIENTMODE,	# Dedicated Server Mode
 	NOTSTARTED = -1
 }
 
+const NETWORKPORT : int = 21277
+const NETWORKSERVERMAXPEERS : int = 4
+
 @onready var GlobalNetworkMode : NetworkMode = NetworkMode.NOTSTARTED
+
+##UI
+var UIDebugLabel :Node
+
+
+func _ready() -> void:
+	UIDebugLabel = get_tree().current_scene.get_node("CanvasLayerUI/Debug/Label")
+	GlobalSignals.networkmodechanged.connect(OnNetworkModeChange)
+
+
+func OnNetworkModeChange(mode : NetworkMode) -> void:
+	print("Signal > GlobalData: NetworkModeChanges %s" % str(mode))
+	GlobalNetworkMode = mode
