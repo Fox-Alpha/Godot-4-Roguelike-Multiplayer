785ad9d3e74c2f5d37a1828a1a164e25094bb186
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-07 22:54:54 +0100

    Add: Using global Datea and signals Add: Change NetworkMode Signal Add: error handling on creation Add: connection failed output message ===: Set debug output on label

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scripts/network/client_manager.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scripts/network/client_manager.gd
@@ -3,7 +3,7 @@ class_name Client_Manager extends Node
 #[Export] private string _address = "localhost";
 @export var _adress : String = "localhost"
 #[Export] private int _port = 9999;
-@export var _port : int = 9999
+@export var _port : int = 21277
 
 #private SceneMultiplayer _multiplayer = new();
 @onready var _multiplayer : SceneMultiplayer = SceneMultiplayer.new()
@@ -16,8 +16,13 @@ class_name Client_Manager extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_Connect()
-	pass # Replace with function body.
+
+	if _Connect():
+		printerr("Error durring Server creation !")
+	else:
+		print("Client Ready !")
+		GlobalSignals.clientcreated.emit()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,22 +30,31 @@ func _process(_delta: float) -> void:
 	pass
 
 
-func _Connect() -> void:
-	#_multiplayer.ConnectedToServer += OnConnectedToServer;
-	_multiplayer.connected_to_server.connect(OnConnectedToServer)
-	#_multiplayer.PeerPacket += OnPacketReceived;
+func _Connect() -> bool:
+		#_multiplayer.ConnectedToServer += OnConnectedToServer;
+		#_multiplayer.PeerPacket += OnPacketReceived;
+		#_multiplayer.ServerDisconnected += OnServerDisconnected;
+		#_multiplayer.ConnectionFailed += OnConnectionFailed;
+		#_multiplayer.PeerConnected += OnPeerConnected;
+		#_multiplayer.PeerDisconnected += OnPeerDisconnected;
 
-	#_multiplayer.ServerDisconnected += OnServerDisconnected;
+	_multiplayer.connected_to_server.connect(OnConnectedToServer)
+	_multiplayer.connection_failed.connect(OnConnectionFailed)
 	_multiplayer.server_disconnected.connect(OnServerDisconnect)
 
-	#ENetMultiplayerPeer peer = new();
 	var peer : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
-	#peer.CreateClient(_address, _port);
-	peer.create_client(_adress, _port)
-	_multiplayer.multiplayer_peer = peer;
 
-	var path = get_path()
-	get_tree().set_multiplayer(_multiplayer, path)
+	var error = peer.create_client(_adress, GlobalData.NETWORKPORT)
+	if error != OK:
+		print("Error during Server creation %d " % error_string(error))
+		GlobalSignals.networkmodechanged.emit(GlobalData.NetworkMode.NOTSTARTED)
+	else:
+		_multiplayer.multiplayer_peer = peer;
+
+		var path = get_path()
+		get_tree().set_multiplayer(_multiplayer, path)
+
+	return error != OK
 
 func OnConnectedToServer() -> void:
 	print("Client connected to Server:", _adress, ":", _port)
@@ -57,4 +71,11 @@ func OnServerDisconnect() -> void:
 	#GetNode<Label>("Debug/Label").Text = $"\nServer has closed the Connection !\nQuitting in 5 Seconds";
 	#await ToSignal(GetTree().CreateTimer(5), "timeout");
 	#GetTree().Quit();
+	GlobalSignals.DebugLabelText.emit("\nServer has closed the Connection !\nQuitting in 5 Seconds", Color.RED)
+	pass
+
+
+func OnConnectionFailed() -> void:
+	push_error("ClientManager::OnConnectionFailed(): Connecting to: {0}:{1} has failed".format([_adress,_port]))
+	print("ClientManager::OnConnectionFailed(): Connecting to: {0}:{1} has failed".format([_adress,_port]))
 	pass
