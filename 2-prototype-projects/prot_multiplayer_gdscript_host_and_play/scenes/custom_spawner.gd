413b288b5f53cc4fda76280dbb6613afc375a16b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-09 22:25:09 +0100

    Add: minimal spawning for nodes Add: hiding nodes on single / hosting play

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scenes/custom_spawner.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scenes/custom_spawner.gd
@@ -1,14 +1,13 @@
 extends MultiplayerSpawner
 
 const PLAYER = preload("res://objects/entities/player/player.tscn")
+const DUMMY = preload("res://objects/entities/dummy/dummy.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_multiplayer_authority(1)
-	#var customSpawnFunction : Callable = _OnCustomSpawn
 	spawn_function = _OnCustomSpawn
-
-	pass # Replace with function body.
+	set_multiplayer_authority(1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,38 +15,28 @@ func _process(_delta):
 	pass
 
 
-func _OnCustomSpawn(data:int) -> Node:
-	var spawnedPlayerID:int = data
-	var localID : int = multiplayer.get_unique_id()
+func _OnCustomSpawn(data) -> Node:
+	var _spawnedPlayerID:int = data
+	var _localID : int = multiplayer.get_unique_id()
 
 	## DEBUG
-	#print("OnCustomSpawn() : ", multiplayer.get_unique_id(), " : ", data)
 	print(	""">> MultiplayerSpawner::CustomSpawnFunction():
 			Local UniqueId: ({%d}
 			Authority: {%d})""" % [multiplayer.get_unique_id(), get_multiplayer_authority()]);
 
+	#push_warning(">> MultiplayerSpawner::CustomSpawnFunction():
+			#\tLocal UniqueId: ({%d}
+			#\tAuthority: {%d})" % [multiplayer.get_unique_id(), get_multiplayer_authority()]);
 
-	if localID == 1:					# is local Server
-		print("Local Server")
-		var server : Node = Node.new()
-		server.name = str(data)
-
-		server.set_multiplayer_authority(1)
-		return server
-	elif localID == spawnedPlayerID:	# is local Player
-		print("local player")
-		pass
-	else:								# all other peers
-		print("other peer")
-		pass
-
-	var player : CharacterBody2D = PLAYER.instantiate()
-	player.name = str(data)
-	#player.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
-
+	var dummy : Node2D = DUMMY.instantiate()
+	dummy.name = str(data)
 	var pos = get_tree().current_scene.get_viewport().get_visible_rect().get_center()
-	player.global_position = pos
+	dummy.global_position = pos
+	if _localID == 1:
+		dummy.hide()
 
-	player.set_multiplayer_authority(data)
+	dummy.set_multiplayer_authority(data)
 
-	return player
+	return dummy
+
+
