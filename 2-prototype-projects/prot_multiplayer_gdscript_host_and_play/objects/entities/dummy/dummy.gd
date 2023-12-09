f902b6f9385ebf5d4ba8cca1afcb614c051f11f1
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-09 20:12:38 +0100

    Add: new dummy scene for spawning entities Add: Changing color for every node Add: Name label

---
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/objects/entities/dummy/dummy.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+const SPEED := 500.0
+const ACCELERATION: float = 15.0
+@export var direction := Vector2.ZERO
+
+var player_name: Label
+
+#@export var velocity : Vector2 = Vector2.ZERO
+#@export var position : Vector2 = Vector2.ZERO
+func _ready() -> void:
+	player_name = $PlayerName
+	player_name.text = name
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+	if !is_multiplayer_authority():
+		return
+
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	print("Player Direction: ", direction)
+	pass
+
+
+func _process(_delta: float) -> void:
+	#if !multiplayer.is_server():
+	if is_multiplayer_authority():
+		velocity.x = move_toward(velocity.x, SPEED * direction.x, ACCELERATION)
+		velocity.y = move_toward(velocity.y, SPEED * direction.y, ACCELERATION)
+	#if multiplayer.is_server():
+		move_and_slide()
+
+
+func _enter_tree() -> void:
+	if multiplayer.get_unique_id() == multiplayer.multiplayer_peer.TARGET_PEER_SERVER:
+		get_node("Icon").self_modulate = Color.LAWN_GREEN
+	else:
+		get_node("Icon").self_modulate = randomize_modulation()
+
+
+
+
+func randomize_modulation() -> Color:
+	var color : Color = Color(
+		randf_range(0.0, 1.0),
+		randf_range(0.0, 1.0),
+		randf_range(0.0, 1.0),
+		1
+	)
+	return color
