8d1c200cc38ff9fc454afd8d7764db4d4ab27863
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-27 18:15:35 +0100

    Add: Setting Maintitle on NetworkModeChange signal Add: Setting Label Text on NetworkModeChange signal

--- 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scripts/globals/global_data.gd
+++ 2-prototype-projects/prot_multiplayer_gdscript_host_and_play/scripts/globals/global_data.gd
@@ -33,20 +33,49 @@ var _GlobalNetworkMode :  NetworkMode :
 
 ##UI
 var UIDebugLabel :Node
+var UICtrllabel : Node
 
 
 func _ready() -> void:
 	GodotLogger._prefix = "GLOBAL"
 	MAINLOG = GodotLogger.with("GlobalLogger")
 
+	UIDebugLabel = get_tree().current_scene.get_node_or_null("CanvasLayerUI/Debug/Label")
+	UICtrllabel =  get_tree().current_scene.get_node_or_null("CanvasLayerUI/Control/Label")
+
 	_GlobalNetworkMode = NetworkMode.NOTSTARTED
-	UIDebugLabel = get_tree().current_scene.get_node("CanvasLayerUI/Debug/Label")
 	GlobalSignals.networkmodechanged.connect(OnNetworkModeChange)
 
 
 func OnNetworkModeChange(mode : NetworkMode) -> void:
 	MAINLOG.info("%sNetworkModeChanges %s" % [_MSGPREFIX, str(mode)])
 	_GlobalNetworkMode = mode
+	var title : String = ""
+
+	match GetGlobalNetworkMode() as NetworkMode:
+		NetworkMode.NOTSTARTED:			# Main Menu
+			print("")
+		NetworkMode.SINGLEPLAYERMODE:		# Local Singleplayer Mode
+			GlobalData.UICtrllabel.text = "Standalone Side"
+			title = "Single Player"
+			print("")
+		NetworkMode.CLIENTONLYMODE:			# Client Only
+			GlobalData.UICtrllabel.text = "Client Side"
+			title = "Client"
+			print("")
+		NetworkMode.SERVERONLYMODE:			# Dedicated Server
+			GlobalData.UICtrllabel.text = "Server Side"
+			title = "Dedicated Server"
+			print("")
+		NetworkMode.SERVERCLIENTMODE:		# Host and Play Mode
+			GlobalData.UICtrllabel.text = "Host & Play"
+			title = "Host and Play Mode"
+			print("")
+		_:
+			print("")
+
+	GlobalSignals.MainTitleChanged.emit(title)
+
 
 
 func GetGlobalNetworkMode() -> NetworkMode:
