f14edb5f38ffb20dff9b5d9dceb3e9d37a320d95
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-30 17:37:59 +0100

    Upd: Refactor wall auto creation ===: Setup default values ===: Optimize call to func

--- 2-prototype-projects/prot_godot_4_3_dev1/Walls.gd
+++ 2-prototype-projects/prot_godot_4_3_dev1/Walls.gd
@@ -7,6 +7,33 @@ enum WallDirection{
 	SOUTH
 }
 
+var wallsettings : Dictionary = {
+	"NORTH": {
+		"wallname": "WALL_NORTH",
+		"wallsize": Vector2i(0, 16),
+		"wallposition": Vector2i(0, 0),
+		"wallrotation": 0
+	},
+	"EAST": {
+		"wallname": "WALL_EAST",
+		"wallsize": Vector2i(0, 16),
+		"wallposition": Vector2i(vpsize.x, 0),
+		"wallrotation": 90
+	},
+	"SOUTH": {
+		"wallname": "WALL_SOUTH",
+		"wallsize": Vector2i(0, 16),
+		"wallposition": Vector2i(vpsize.x, 0),
+		"wallrotation": 180
+	},
+	"WEST": {
+		"wallname": "WALL_WEST",
+		"wallsize": Vector2i(0, 16),
+		"wallposition": vpsize,
+		"wallrotation": -90
+	},
+}
+
 @export_category("Walls Setup")
 @export var walls : Array[PackedScene]
 
@@ -15,6 +42,20 @@ enum WallDirection{
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	printt("ViewPort Size: ", vpsize)
+	print("Screen Center: (%s / %s)" % [vpsize.x / 2.0, vpsize.y / 2.0])
+	
+	wallsettings["NORTH"]["wallposition"] = Vector2i(0, 0)
+	wallsettings["EAST"]["wallposition"] = Vector2i(vpsize.x, 0)
+	wallsettings["SOUTH"]["wallposition"] = vpsize
+	wallsettings["WEST"]["wallposition"] = Vector2i(0,vpsize.y)
+
+	wallsettings["NORTH"]["wallsize"] = Vector2i(vpsize.x, 16)
+	wallsettings["EAST"]["wallsize"] = Vector2i(vpsize.y, 16)
+	wallsettings["SOUTH"]["wallsize"] = Vector2i(vpsize.x, 16)
+	wallsettings["WEST"]["wallsize"] = Vector2i(vpsize.y, 16)
+	
+	#print(wallsettings)
+	
 	createwall(WallDirection.NORTH)
 	createwall(WallDirection.EAST)
 	createwall(WallDirection.SOUTH)
@@ -28,36 +69,17 @@ func _ready():
 
 func createwall(dir : WallDirection):
 	var wall : StaticBody2D = walls[dir].instantiate()
-	var wallsize : Vector2i = Vector2i.ZERO
-	var wallpos : Vector2i = Vector2i.ZERO
-	var wallrotation : int = 0
 	var wallname : String = WallDirection.keys()[dir]
 	
 	match dir:
 		WallDirection.NORTH:
-			if wall.has_method("SetWallPosition"):
-				wallsize = Vector2i(vpsize.x, 16)
-				wallpos = Vector2i(0,0)
-				wallrotation = 0
-				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
+				var _x = wall.call_deferred("SetWallPositionDict",wallsettings[wallname]) if wall.has_method("SetWallPositionDict") else false
 		WallDirection.EAST:
-			if wall.has_method("SetWallPosition"):
-				wallsize = Vector2i(vpsize.y, 16)
-				wallpos = Vector2i(vpsize.x,0)
-				wallrotation = 90
-				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
+				var _x = wall.call_deferred("SetWallPositionDict",wallsettings[wallname]) if wall.has_method("SetWallPositionDict") else false
 		WallDirection.SOUTH:
-			if wall.has_method("SetWallPosition"):
-				wallsize = Vector2i(vpsize.x, 16)
-				wallpos = vpsize
-				wallrotation = 180
-				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
+				var _x = wall.call_deferred("SetWallPositionDict",wallsettings[wallname]) if wall.has_method("SetWallPositionDict") else false
 		WallDirection.WEST:
-			if wall.has_method("SetWallPosition"):
-				wallsize = Vector2i(vpsize.y, 16)
-				wallpos = Vector2i(0,vpsize.y)
-				wallrotation = -90
-				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
+				var _x = wall.call_deferred("SetWallPositionDict",wallsettings[wallname]) if wall.has_method("SetWallPositionDict") else false
 
 	add_child(wall)
 	pass
--- 2-prototype-projects/prot_godot_4_3_dev1/wall.gd
+++ 2-prototype-projects/prot_godot_4_3_dev1/wall.gd
@@ -1,8 +1,10 @@
 extends StaticBody2D
 
+@onready var vpsize : Vector2i = get_tree().current_scene.get_viewport().get_visible_rect().size
+
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
+func _ready():
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -10,13 +12,10 @@ extends StaticBody2D
 	#pass
 
 
-func SetWallPosition(dir: String, wallsize: Vector2i, wallpos: Vector2i, wallrotation: int):
-	name = "WALL_%s" % dir
-	get_node_or_null("Texture").size = wallsize
-	global_position = wallpos
-	rotation_degrees = wallrotation
-	
-	printt("Wall %s GlobalPosition: %s" % [name, global_position])
-	printt("Wall %s Size: %s" % [name, find_child("Texture").size])
-	printt("Wall %s Rotation: %s" % [name, rotation_degrees])
-	pass
+func SetWallPositionDict(data: Dictionary):
+	name = data["wallname"]
+	get_node_or_null("Texture").size = data["wallsize"]
+	global_position = data["wallposition"]
+	rotation_degrees = data["wallrotation"]
+	printt("Wall %s GlobalPosition: %s / Size: %s / Rotation: %s" % [name, global_position, find_child("Texture").size, rotation_degrees])
+	return true
