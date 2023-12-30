4566391d8565d3e948bb180d431f1b5f7c27c577
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-30 13:53:47 +0100

    Mve: move the wall settings method to the wall itself

--- 2-prototype-projects/prot_godot_4_3_dev1/Walls.gd
+++ 2-prototype-projects/prot_godot_4_3_dev1/Walls.gd
@@ -10,15 +10,15 @@ enum WallDirection{
 @export_category("Walls Setup")
 @export var walls : Array[PackedScene]
 
-@onready var vpsize : Vector2 = get_tree().current_scene.get_viewport().get_visible_rect().size
+@onready var vpsize : Vector2i = get_tree().current_scene.get_viewport().get_visible_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	printt("VP Size: ", vpsize)
-	setwall(WallDirection.NORTH)
-	setwall(WallDirection.EAST)
-	setwall(WallDirection.SOUTH)
-	setwall(WallDirection.WEST)
+	printt("ViewPort Size: ", vpsize)
+	createwall(WallDirection.NORTH)
+	createwall(WallDirection.EAST)
+	createwall(WallDirection.SOUTH)
+	createwall(WallDirection.WEST)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,34 +26,38 @@ func _ready():
 	#pass
 
 
-func setwall(dir : WallDirection):
-	var w : StaticBody2D = walls[dir].instantiate()
-	w.name = WallDirection.keys()[dir]
+func createwall(dir : WallDirection):
+	var wall : StaticBody2D = walls[dir].instantiate()
+	var wallsize : Vector2i = Vector2i.ZERO
+	var wallpos : Vector2i = Vector2i.ZERO
+	var wallrotation : int = 0
+	var wallname : String = WallDirection.keys()[dir]
+	
 	match dir:
 		WallDirection.NORTH:
-			w.find_child("Texture").size.x = vpsize.x
-			w.global_position.x = 0
-			w.global_position.y = 0
-			w.global_rotation = 0
+			if wall.has_method("SetWallPosition"):
+				wallsize = Vector2i(vpsize.x, 16)
+				wallpos = Vector2i(0,0)
+				wallrotation = 0
+				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
 		WallDirection.EAST:
-			w.find_child("Texture").size.x = vpsize.y
-			w.global_position.x = vpsize.x
-			w.global_position.y = 0
-			w.rotation_degrees = 90
+			if wall.has_method("SetWallPosition"):
+				wallsize = Vector2i(vpsize.y, 16)
+				wallpos = Vector2i(vpsize.x,0)
+				wallrotation = 90
+				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
 		WallDirection.SOUTH:
-			w.rotation_degrees = 180
-			w.find_child("Texture").size.x = vpsize.x
-			w.global_position = vpsize
+			if wall.has_method("SetWallPosition"):
+				wallsize = Vector2i(vpsize.x, 16)
+				wallpos = vpsize
+				wallrotation = 180
+				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
 		WallDirection.WEST:
-			w.rotation_degrees = -90
-			w.rotate
-			w.find_child("Texture").size.x = vpsize.y
-			w.global_position.x = 0
-			w.global_position.y = vpsize.y
+			if wall.has_method("SetWallPosition"):
+				wallsize = Vector2i(vpsize.y, 16)
+				wallpos = Vector2i(0,vpsize.y)
+				wallrotation = -90
+				wall.call_deferred("SetWallPosition", wallname, wallsize, wallpos, wallrotation)
 
-	printt("Wall %s GlobalPosition: %s" % [w.name, w.global_position])
-	printt("Wall %s Width: %s" % [w.name, w.find_child("Sprite2D").texture.width])
-	printt("Wall %s Height: %s" % [w.name, w.find_child("Sprite2D").texture.height])
-
-	add_child(w)
+	add_child(wall)
 	pass
--- 2-prototype-projects/prot_godot_4_3_dev1/wall.gd
+++ 2-prototype-projects/prot_godot_4_3_dev1/wall.gd
@@ -1,6 +1,5 @@
 extends StaticBody2D
 
-
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 	#pass # Replace with function body.
@@ -9,3 +8,15 @@ extends StaticBody2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
+
+
+func SetWallPosition(dir: String, wallsize: Vector2i, wallpos: Vector2i, wallrotation: int):
+	name = "WALL_%s" % dir
+	get_node_or_null("Texture").size = wallsize
+	global_position = wallpos
+	rotation_degrees = wallrotation
+	
+	printt("Wall %s GlobalPosition: %s" % [name, global_position])
+	printt("Wall %s Size: %s" % [name, find_child("Texture").size])
+	printt("Wall %s Rotation: %s" % [name, rotation_degrees])
+	pass
