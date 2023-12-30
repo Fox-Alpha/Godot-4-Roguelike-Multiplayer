97b0d471ec571d39a7053f2e35cf2cb1df9cc9d1
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-30 17:36:42 +0100

    Upd: Cleanup source

--- 2-prototype-projects/prot_godot_4_3_dev1/centernode2d.gd
+++ 2-prototype-projects/prot_godot_4_3_dev1/centernode2d.gd
@@ -2,16 +2,16 @@ extends Node
 
 @export var NodesToCenter : Array[Node]
 @onready var screencenter = get_tree().current_scene.get_viewport().get_visible_rect().get_center()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var ChildsToCenter = get_children()
 	
 	for c in ChildsToCenter:
 		(c as Node2D) .global_position = screencenter
-		print("Screen Center: %s" % screencenter)
 		print("Node %s Position: %s" % [c.name, c.global_position])
 
 	for c in NodesToCenter:
 		(c as Node2D) .global_position = screencenter
-		print("Screen Center: %s" % screencenter)
 		print("Node %s Position: %s" % [c.name, c.global_position])
