dc18da543c62ff9bd3e7bb57ab33cacff33b3dcf
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-12-31 01:01:04 +0100

    Add: Camscript for limit the cam and follow the player

---
+++ 2-prototype-projects/prot_godot_4_3_dev1/player_cam2d.gd
@@ -0,0 +1,19 @@
+extends Camera2D
+
+var map : TileMap
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	map = get_tree().current_scene.get_node_or_null("TileMap")
+	var mapRect : Rect2i = map.get_used_rect()
+	var tileSize = 64
+	var worldSizeInPixels : Vector2i = mapRect.size * tileSize
+	print(worldSizeInPixels)
+	#limit_top = worldSizeInPixels.x
+	#limit_bottom = worldSizeInPixels.y
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
